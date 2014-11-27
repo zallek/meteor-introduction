d0a4655951d52fdca87f89b9169cd359ea6543fd
zallek <nicolas.fortin37@gmail.com>
2014-11-27 13:47:44 +0100

    step 5

--- simple_chat/server/publish.js
+++ simple_chat/server/publish.js
@@ -1,3 +1,12 @@
 Meteor.publish("posts", function(){
 	return Posts.find();
 });
+
+Meteor.allow({
+	insert: function(userId, message){
+		return true;
+	},
+	remove: function(userId){
+		return true;
+	},
+});
